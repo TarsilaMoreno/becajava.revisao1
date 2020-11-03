b201d25e2e93a007c9e9e46ed57827b7fc92e0a0
Tarsila Muriel Moreno <tmurielm@everis.com>
2020-11-02 22:57:06 -0300

    first commit

---
+++ PROCEDURE_Atualizar_Fornecedor_Cnpj.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE SP_Atualizar_Fornecedor_Cnpj
+	@id INT,
+	@cnpj VARCHAR(100)
+	
+AS
+BEGIN
+	UPDATE dbo.fornecedor SET
+	cnpj = @cnpj
+	WHERE id = @id
+END
+
+EXEC SP_Atualizar_Fornecedor_Cnpj 2,'23.456.789/0001-13'
+
+SELECT * FROM fornecedor
